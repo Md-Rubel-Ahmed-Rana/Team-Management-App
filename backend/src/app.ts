79993b41a8ae8d489d38f321f93564d0aab59576
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-07-03 18:24:08 +0600

    CORS origin enabled public

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,12 +26,7 @@ const io = new Server(server, {
   },
 });
 
-app.use(
-  cors({
-    origin: ["https://team-manager-eight.vercel.app"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
