511dba2a22a1d1c127454631f1c7939a226488e1
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-07-06 12:51:15 +0600

    Cookie parsing removed from client side

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,28 +1,10 @@
 import Dashboard from "@/components/pages/dashboard";
 import RootLayout from "@/layout/RootLayout";
 import { NextPageWithLayout } from "pages/_app";
-import React, { ReactElement, useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { useRouter } from "next/router";
-import Loader from "@/components/shared/Loader";
+import React, { ReactElement } from "react";
 import GetHead from "@/utils/Head";
 
 const DashboardPage: NextPageWithLayout = () => {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  useEffect(() => {
-    const handleAuth = async () => {
-      setLoading(true);
-      const isLoggedIn = Cookies.get("tmAccessToken");
-      if (isLoggedIn === "undefined") {
-        setLoading(false);
-        return router.push("/login");
-      } else {
-        setLoading(false);
-      }
-    };
-    handleAuth();
-  }, [router]);
   return (
     <div className="py-5">
       <GetHead
@@ -30,7 +12,7 @@ const DashboardPage: NextPageWithLayout = () => {
         description="team management, project collaboration, task tracking, project details"
         keywords="team management, project collaboration, task tracking, project details"
       />
-      {loading ? <Loader /> : <Dashboard />}
+      <Dashboard />
     </div>
   );
 };
--- frontend/src/pages/teams/[id].tsx
+++ frontend/src/pages/teams/[id].tsx
@@ -2,28 +2,10 @@ import TeamDetailsPage from "@/components/pages/teams/showTeam/TeamDetailsPage";
 import RootLayout from "@/layout/RootLayout";
 import { NextPageWithLayout } from "pages/_app";
 import React, { ReactElement, useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { useRouter } from "next/router";
-import Loader from "@/components/shared/Loader";
 import GetHead from "@/utils/Head";
 import TeamDetailsMobilePage from "@/components/pages/teams/mobile";
 
 const TeamDetails: NextPageWithLayout = () => {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  useEffect(() => {
-    const handleAuth = async () => {
-      setLoading(true);
-      const isLoggedIn = Cookies.get("tmAccessToken");
-      if (!isLoggedIn) {
-        setLoading(false);
-        return router.push("/login");
-      } else {
-        setLoading(false);
-      }
-    };
-    handleAuth();
-  }, [router]);
   return (
     <div className="py-5">
       <GetHead
@@ -31,18 +13,14 @@ const TeamDetails: NextPageWithLayout = () => {
         description="team management, project collaboration, task tracking, project details"
         keywords="team management, project collaboration, task tracking, project details"
       />
-      {loading ? (
-        <Loader />
-      ) : (
-        <div>
-          <div className="hidden lg:block">
-            <TeamDetailsPage />
-          </div>
-          <div className="md:hidden">
-            <TeamDetailsMobilePage />
-          </div>
+      <div>
+        <div className="hidden lg:block">
+          <TeamDetailsPage />
         </div>
-      )}
+        <div className="md:hidden">
+          <TeamDetailsMobilePage />
+        </div>
+      </div>
     </div>
   );
 };
