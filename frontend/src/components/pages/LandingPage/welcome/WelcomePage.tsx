697836432dd44636d69595dcbf2afb7f423d2165
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-08-25 14:08:28 +0600

    Banner and Timeline section added on homepage

--- frontend/src/components/pages/LandingPage/welcome/WelcomePage.tsx
+++ frontend/src/components/pages/LandingPage/welcome/WelcomePage.tsx
@@ -3,21 +3,26 @@ import React from "react";
 
 const WelcomePage = () => {
   return (
-    <section className="lg:py-36 py-24 px-2 text-center">
-      <h1 data-aos="fade-up" className="lg:text-4xl text-2xl font-bold mb-4">
-        Welcome to Team Manager
-      </h1>
-      <p data-aos="fade-up" className="text-lg mb-8">
-        Effortlessly manage tasks, projects, and collaboration with Team
-        Manager.
-      </p>
-      <button
-        data-aos="fade-up"
-        className="bg-purple-700 text-white py-2 px-4 rounded-full hover:bg-purple-900 hover:text-white"
-      >
-        <Link href={"/signup"}>Get Started</Link>
-      </button>
-    </section>
+    <div className="text-center bg-black bg-opacity-50 absolute top-0 w-full h-full flex justify-center items-center">
+      <div className="px-4 py-6 rounded-lg">
+        <h1
+          data-aos="fade-left"
+          className="lg:text-4xl text-xl font-bold mb-4 text-white"
+        >
+          Welcome to Team Manager
+        </h1>
+        <p data-aos="fade-right" className="text-lg mb-8 text-white">
+          Effortlessly manage tasks, projects, and collaboration with Team
+          Manager.
+        </p>
+        <button
+          data-aos="fade-up"
+          className="bg-purple-700 text-white py-2 px-4 rounded-full hover:bg-purple-900 hover:text-white"
+        >
+          <Link href={"/signup"}>Get Started</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
---
+++ frontend/src/constants/bannerCarouselData.ts
@@ -0,0 +1,34 @@
+export const carouselItems = [
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566331/team-manager/assets/team-manager-bg-slider-image-2_ygtv5g.jpg",
+    alt: "Slide 1",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566330/team-manager/assets/team-manager-bg-slider-image-6_igzrwo.jpg",
+    alt: "Slide 2",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566324/team-manager/assets/team-manager-bg-slider-image-8_d0bf1i.jpg",
+    alt: "Slide 3",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566324/team-manager/assets/team-manager-bg-slider-image-4_kiungc.jpg",
+    alt: "Slide 4",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566321/team-manager/assets/team-manager-bg-slider-image-5_eaanyv.jpg",
+    alt: "Slide 5",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566319/team-manager/assets/team-manager-bg-slider-image-3_um8wrj.jpg",
+    alt: "Slide 6",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566319/team-manager/assets/team-manager-bg-slider-image-1_wswquc.jpg",
+    alt: "Slide 7",
+  },
+  {
+    src: "https://res.cloudinary.com/dy4qhabxk/image/upload/v1724566315/team-manager/assets/team-manager-bg-slider-image-7_bxzde9.jpg",
+    alt: "Slide 8",
+  },
+];
